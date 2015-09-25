a5de4cd7a253a67a5cf07b8bbce2e8e3a251ad5a
natthapong manosri <natthaphong.man@crru.ac.th>
2015-09-25 14:38:58 +0700

    Updata java file...

--- Array01.java
+++ Array01.java
@@ -1,15 +1,14 @@
+import java.util.Scanner;
 public class Array01 {
   public static void main (String args[]) {
-		int[ ] a = new int [8];
-	a[0] = 1;
-	a[1] = 2;
-	a[2] = 3;
-	a[3] = 4;
-	a[4] = 5;
-	a[5] = 6;
-	a[6] = 7;
-	a[7] = 8;
-	System.out.println("A= " +a);
+	  
+	  Scanner input = new Scanner(System.in);
+	  System.out.print("Insert Your Num : ");
+	  int num =input.nextInt();
+      int[] a = new int[num];
+	  for (int i = 1 ; i <= a.length ; i++){
+		  System.out.println("A = "+i);
+	  }
       
   }
 }
