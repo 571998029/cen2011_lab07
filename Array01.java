0c3092113eee9e3d685593a4c1b515ca97802fdf
natthapong manosri <natthaphong.man@crru.ac.th>
2015-09-25 14:26:38 +0700

    Updata java file...

--- Array01.java
+++ Array01.java
@@ -1,9 +1,15 @@
 public class Array01 {
   public static void main (String args[]) {
-      int[] a;
+		int[ ] a = new int [8];
+	a[0] = 1;
+	a[1] = 2;
+	a[2] = 3;
+	a[3] = 4;
+	a[4] = 5;
+	a[5] = 6;
+	a[6] = 7;
+	a[7] = 8;
+	System.out.println("A= " +a);
       
-      // Assign Value to A
-      
-      // Print a Array
   }
 }
